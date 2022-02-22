89fc69092f4c93c7e87fa890e228d0cbbd25efc2
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-02-22 14:58:09 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,25 +3,6 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
-/*
-TEST_CASE("infers the breach according to limits") {
-  REQUIRE(inferBreach(PASSIVE_COOLING,-1) == TOO_LOW);
-  REQUIRE(inferBreach(PASSIVE_COOLING,36) == TOO_HIGH);
-  REQUIRE(inferBreach(PASSIVE_COOLING,0) == NORMAL);
-  REQUIRE(inferBreach(PASSIVE_COOLING,35) == NORMAL);
-  
-  REQUIRE(inferBreach(HI_ACTIVE_COOLING,-1) == TOO_LOW);
-  REQUIRE(inferBreach(HI_ACTIVE_COOLING,46) == TOO_HIGH);
-  REQUIRE(inferBreach(HI_ACTIVE_COOLING,0) == NORMAL);
-  REQUIRE(inferBreach(HI_ACTIVE_COOLING,45) == NORMAL);
-  
-  REQUIRE(inferBreach(MED_ACTIVE_COOLING,-1) == TOO_LOW);
-  REQUIRE(inferBreach(MED_ACTIVE_COOLING,41) == TOO_HIGH);
-  REQUIRE(inferBreach(MED_ACTIVE_COOLING,0) == NORMAL);
-  REQUIRE(inferBreach(MED_ACTIVE_COOLING,40) == NORMAL);
-}
-*/
-
 TEST_CASE("Check and alert if there is a breach in Passive cooling via controller") {
   AlertTarget alertTarget = TO_CONTROLLER;
   BatteryCharacter batteryChar = {PASSIVE_COOLING,"BatteryBrandXYZ"};
